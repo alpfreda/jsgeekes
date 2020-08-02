e9dd0f04c2735711a8b5ab3c519ad1d822be8053
Mehdi AK <aspmvc7@gmail.com>
2020-08-02 20:52:19 +0300

    fixed bug in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Header } from './components/header';
+import { Header } from './components/header/Index';
 import { Home } from './pages/Home';
 
 export const App: React.FC<{}> = () => (
