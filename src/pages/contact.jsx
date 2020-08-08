dc2738818f0c29251fca1ad431bb3637f40ca67d
Mehdi AK <aspmvc7@gmail.com>
2020-08-08 22:42:18 +0300

    fixed resume page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -11,23 +11,23 @@ const Contact = () => {
     return <section className="contact-page">
         <div className="container">
             <div className="contact-page-inner">
-                <h1 className="page-title">
+                <h1 className="page-title ">
                     Contact Me
                     </h1>
                 <p className="hear-info">I am so glad to hear from you  🙂</p>
-                <form>
+                <form netlify>
                     <div className='form-input'>
-                        <input type='text' className='input' required />
+                        <input type='text' name='name' className='input' required />
                         <span className="inputBar"></span>
                         <label>Name</label>
                     </div>
                     <div className='form-input'>
-                        <textarea className='input' cols="30" rows="10" required></textarea>
+                        <textarea className='input' name='note' cols="30" rows="10" required></textarea>
                         <span className="inputBar"></span>
                         <label>Note</label>
                     </div>
                     <div className='text-right'>
-                        <button className='btn-submit'>Send Message</button>
+                        <button type="submit" className='btn-submit'>Send Message</button>
                     </div>
                 </form>
             </div>
